5abc8d8257400beb3c670062fa107a0bbf1b1fea
lilianDinh <202107277@post.au.dk>
2024-03-13 00:07:15 +0100

    Opgave4 - main rettelser

--- Opgave4/main.cpp
+++ Opgave4/main.cpp
@@ -1,7 +1,7 @@
 #include "MinHeapPriorityQueue.cpp"
 
 int main() {
-    vector<int> elements = {25, 5, 15, 10, 20};
+    vector<int> elements = {25, 5, 15, 10, 30};
     MinHeapPriorityQueue<int> pq(elements);
 
     cout << "Elements:" << endl;
@@ -10,9 +10,9 @@ int main() {
     }
     cout << endl;
 
-    cout << endl<< "New elements: 30, 0" << endl;
-    pq.push(30);
-    pq.push(0);
+    cout << endl<< "New elements 30 and 35" << endl;
+    pq.push(20);
+    pq.push(35);
 
     cout << endl<< "Top: " << pq.top() << endl;
     pq.pop();
